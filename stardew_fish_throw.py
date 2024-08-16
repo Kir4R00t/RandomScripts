4ebdbf5a03c70b96cd76f02f15703ff12075e810
Kir4R00t <miki4sa@interia.pl>
2024-08-16 23:35:59 +0200

    perfect fish throw

---
+++ stardew_fish_throw.py
@@ -0,0 +1,16 @@
+import pyautogui as pg
+from pynput.keyboard import Key, Listener
+from time import sleep
+
+def throw_fish(key):
+    if key == key.f1:
+        pg.mouseDown(button='left')
+        sleep(0.956) # figure how long
+        pg.mouseUp(button='left')
+
+def main():
+    with Listener(on_press=lambda key: throw_fish(key),) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
